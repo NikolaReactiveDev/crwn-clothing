7a265e7076c72a7f4c9a059a7c3f156bbfd93f51
Nikola <61063509+NikolaReactiveDev@users.noreply.github.com>
2020-02-29 17:28:16 +0100

    added header component with logo and links to home, shop and contact

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch} from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
- 
+
 
 function App() {
   return (
     <div>
+        <Header />
         <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route path='/shop' component={ShopPage}/>
