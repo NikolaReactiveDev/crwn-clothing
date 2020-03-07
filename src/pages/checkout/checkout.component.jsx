e6a2e6ed090d713d7b32757270c6e92d973b1263
Nikola <61063509+NikolaReactiveDev@users.noreply.github.com>
2020-03-08 00:23:27 +0100

    implementing stripe frontend payment and updating favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,6 +6,7 @@ import { createStructuredSelector} from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors.js';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component.jsx';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component.jsx';
 
 const CheckoutPage = ({cartItems, total}) => {
     return (
@@ -33,6 +34,12 @@ const CheckoutPage = ({cartItems, total}) => {
             <div className="total">
                 <span>Total: ${total}</span>
             </div>
+            <div className="test-warning">
+                ***Please use the following test credit card for payment***
+                <br/>
+                4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+            </div>
+            <StripeCheckoutButton price={total}/>
         </div>
     )
 }
