2c1eaac93b9aee71c63f14c08b58babd43b2c938
Nikola <61063509+NikolaReactiveDev@users.noreply.github.com>
2020-03-15 08:30:09 +0100

    switched from observer to promise patter with firebase in shop componentDidMount method

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -26,11 +26,11 @@ class ShopPage extends Component {
         const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
 
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
-            updateCollections(collectionsMap)
-            this.setState({ loading: false})
-        })
+        collectionRef.get().then(snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
+            this.setState({ loading: false});
+        });
     }
     render(){
         const { match } = this.props;
